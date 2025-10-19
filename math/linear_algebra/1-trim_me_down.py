98a9adeade0e9d2f81d25ba79a1571caac3a587b
sindi16 <sindibardhi078@gmail.com>
2025-10-19 11:43:50 +0200

    correcting exercise 1

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""the_middle should be a 2D matrix containing the 3rd and 4th columns """
+"""Extracts the 3rd and 4th columns from a 2D matrix."""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []
-for row in matrix:
-    the_middle.append(row[2:4])
+the_middle = [row[2:4] for row in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
