903f6b64f15b0266862e44eb1b93551fa6115203
sindi16 <sindibardhi078@gmail.com>
2025-10-20 15:33:02 +0200

    adding exercise 7 to the prject

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
-"""Function to concacenate two matrices
-    along a specific axis"""
+"""Function to concatenate two matrices
+along a specific axis."""
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-
     if axis == 0:
-        if len(mat1[0] != len(mat2[0])):
+        # Vertical concatenation (add rows)
+        if len(mat1[0]) != len(mat2[0]):
             return None
         result = []
         for row in mat1:
-            result.append(row[:])
+            result.append(row[:])  # make a copy of each row
         for row in mat2:
             result.append(row[:])
         return result
-    
-    
-        
 
+    elif axis == 1:
+        # Horizontal concatenation (add columns)
+        if len(mat1) != len(mat2):
+            return None
+        result = []
+        for i in range(len(mat1)):
+            new_row = mat1[i] + mat2[i]
+            result.append(new_row)
+        return result
 
-
-
-
-
-
-    
+    else:
+        return None
