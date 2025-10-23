cf9d1096442e799bed014c69ceda7a12b8715131
sindi16 <10029@holbertonstudents.com>
2025-10-23 10:04:23 +0000

    adding ex.17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
-""""""
+"""Function that calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """Function that calculates the integral of a polynomial"""
+    if type(poly) is not list or len(pol) == 0:
+        return None
+    
+    if type(C) not in (int, float):
+        return None
+    
+    for coeff in poly:
+        if type(coeff) not in (int, float):
+            return None
+    
+    result = [C]
+
+    for i in range(len(poly)):
+        val = poly[i] / (i + 1)
+        if val.is_integer():
+            val = int(val)
+        result.append(val)
+
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
+    
+    return result
