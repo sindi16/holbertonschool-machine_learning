1dcc91eb88d54b663672a9c1ded710550ee8b116
sindi16 <10029@holbertonstudents.om>
2025-10-24 08:55:44 +0000

    starting with poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,1 +1,14 @@
-class
+class Poisson:
+    def __init__(self, data=None, lambtha=1):
+        if lambtha <= 0:
+            raise ValueError:
+            print("lambtha must be a positive value")
+        if type(data) is not list:
+            raise TypeError:
+            print("data must be a list")
+        if data
+        self.__data = __data
+        self.__lambtha = __lambtha
+    @data.setter
+    @Property
+    @lamtha.setter
