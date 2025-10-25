338ac5a0c1155e4ddc5761d700a0712a7795e014
sindi16 <10029@holbertonstudents.com>
2025-10-25 10:11:53 +0000

    adding 1, 2, 3 exercise.

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -7,16 +7,32 @@ class Poisson:
 
     def __init__(self, data=None, lambtha=1.):
         """Class constructor"""
-
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            # Estimate lambtha from data
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the PMF for a given number of successes k"""
+        import math
+        k = int(k)
+        if k < 0:
+            return 0
+        return (math.exp(-self.lambtha) * self.lambtha**k) / math.factorial(k)
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes"""
+        import math
+        k = int(k)
+        if k < 0:
+            return 0
+        cdf_value = 0
+        for i in range(k + 1):
+            cdf_value += self.pmf(i)
+        return cdf_value
