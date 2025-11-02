68230d175f91617a64aa15702f0b840837ac3e41
sindi16 <10029@holbertonstudents.com>
2025-11-02 11:39:37 +0000

    adding ex7

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -17,25 +17,7 @@ df['Date'] = pd.to_datetime(df['Date'], unit='s')
 
 df = df.set_index('Date')
 
-df['Close'].fillna(method='pad', inplace=True)
-df['High'].fillna(df['Close'], inplace=True)
-df['Low'].fillna(df['Close'], inplace=True)
-df['Open'].fillna(df['Close'], inplace=True)
-df['Volume_(BTC)'].fillna(0, inplace=True)
-df['Volume_(Currency)'].fillna(0, inplace=True)
 
-df = df.loc[df.index >= '2017-01-01']
-
-df = df.resample('D').agg({
-    'High': 'max',
-    'Low': 'min',
-    'Open': 'mean',
-    'Close': 'mean',
-    'Volume_(BTC)': 'sum',
-    'Volume_(Currency)': 'sum'
-})
-
-print(df)
 
 df.plot(subplots=True, figsize=(10, 10),
         title='Bitcoin Daily Data (2017 and Beyond)')
--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """A function that takes a pd.DataFrame"""
 
-import pandas as pd
-
 
 def high(df):
     """Sorts it by the High price in descending order."""
