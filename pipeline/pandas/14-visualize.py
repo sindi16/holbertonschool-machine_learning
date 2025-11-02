8142b4b901ddeb504372719796675bee5852e2b9
sindi16 <10029@holbertonstudents.com>
2025-11-02 11:33:27 +0000

    adding ex14

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3
+"""Visualize Coinbase Bitcoin daily data from 2017 onward."""
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
 from_file = __import__('2-from_file').from_file
+
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# YOUR CODE HERE
+df = df.drop(columns=['Weighted_Price'])
+
+df = df.rename(columns={'Timestamp': 'Date'})
+
+df['Date'] = pd.to_datetime(df['Date'], unit='s')
+
+df = df.set_index('Date')
+
+df['Close'].fillna(method='pad', inplace=True)
+df['High'].fillna(df['Close'], inplace=True)
+df['Low'].fillna(df['Close'], inplace=True)
+df['Open'].fillna(df['Close'], inplace=True)
+df['Volume_(BTC)'].fillna(0, inplace=True)
+df['Volume_(Currency)'].fillna(0, inplace=True)
+
+df = df.loc[df.index >= '2017-01-01']
+
+df = df.resample('D').agg({
+    'High': 'max',
+    'Low': 'min',
+    'Open': 'mean',
+    'Close': 'mean',
+    'Volume_(BTC)': 'sum',
+    'Volume_(Currency)': 'sum'
+})
+
+print(df)
+
+df.plot(subplots=True, figsize=(10, 10),
+        title='Bitcoin Daily Data (2017 and Beyond)')
+plt.tight_layout()
+plt.show()
