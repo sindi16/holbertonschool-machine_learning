8bef4cd07d44a3e191c601d12f90ffde18c2895c
sindi16 <10029@holbertonstudents.com>
2025-11-04 14:25:15 +0000

    adding ex 5

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+""" A script that plot a histogram of student scores for a project"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-
+    """ A function that plot a histogram of student scores for a project"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, range=(0, 100) edgecolor='black')
+    # your code here
+    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
     plt.xticks(np.arange(0, 101, step=10))
-    plt.show()
