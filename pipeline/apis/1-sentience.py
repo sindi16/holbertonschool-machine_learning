f8728c2769cbedc379042e87e07afefb8f603f85
sindi16 <10029@holbertonstudents.com>
2025-11-12 19:23:45 +0000

    adding ex 2, returning a list with name of planets where the species are sentient on 2 atrributes

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
-"""create a method that returns the list of names 
-of the home planets of all sentient species."""
+""" Defines methods """
+
+
+import requests
 
 
 def sentientPlanets():
-    """create a method that returns the list of names of planets"""
+    """ Uses the Star Wars API to return the list of home planets """
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
