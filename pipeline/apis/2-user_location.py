41f920924847d6dd0ae54e441d3b0ee13899c1e3
sindi16 <10029@holbertonstudents.com>
2025-11-12 19:44:01 +0000

    adding some el for ex 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-""""""
+""" Uses the GitHub API to print the location of a specific user """
+import requests
+from sys import argv
+from time import time
+
+
+if __name__ == "__main__":
