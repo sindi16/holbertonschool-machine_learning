cbeffc013ad6a03a6772ad2b55233ba6e7de9041
sindi16 <sindibardhi078@gmail.com>
2026-01-13 12:59:49 +0100

    adding ex 3 to project

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+"""Module that defines a Neuron class."""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification."""
+
+    def __init__(self, nx):
+        """
+        Initialize the neuron.
+
+        Args:
+            nx (int): number of input features
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        'Public prop that calculates the forward prop of the neuron'
+        Z = np.matmul(self.W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
