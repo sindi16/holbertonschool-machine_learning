7afd15be31e8beb80b42b179d43f431cd12c9d76
sindi16 <sindibardhi078@gmail.com>
2026-02-07 10:28:39 +0100

    adding ex

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,59 +1,38 @@
 #!/usr/bin/env python3
+"""
+   Adam upgraded
+"""
 
-import matplotlib.pyplot as plt
 import numpy as np
-update_variables_Adam = __import__('9-Adam').update_variables_Adam
 
-def forward_prop(X, W, b):
-    Z = np.matmul(X, W) + b
-    A = 1 / (1 + np.exp(-Z))
-    return A
 
-def calculate_grads(Y, A, W, b):
-    m = Y.shape[0]
-    dZ = A - Y
-    dW = np.matmul(X.T, dZ) / m
-    db = np.sum(dZ, axis=1, keepdims=True) / m
-    return dW, db
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """
+        method that updates variable in place using
+        Adam optimizer algo
 
-def calculate_cost(Y, A):
-    m = Y.shape[0]
-    loss = - (Y * np.log(A) + (1 - Y) * np.log(1 - A))
-    cost = np.sum(loss) / m
+        :param alpha: learning rate
+        :param beta1: weight for first moment
+        :param beta2: weight for second moment
+        :param epsilon: small number to avoid division by zero
+        :param var: ndarray, variable to be updated
+        :param grad: ndarray, gradient of var
+        :param v: previous first moment of var
+        :param s: previous second moment of var
+        :param t: time step used for bias correction
 
-    return cost
+        :return: update var, new first moment, new second moment
+    """
 
-if __name__ == '__main__':
-    lib_train = np.load('Binary_Train.npz')
-    X_3D, Y = lib_train['X'], lib_train['Y'].T
-    X = X_3D.reshape((X_3D.shape[0], -1))
+    # update rules
+    new_v = beta1 * v + (1 - beta1) * grad
+    new_s = beta2 * s + (1 - beta2) * grad**2
 
-    nx = X.shape[1]
-    np.random.seed(0)
-    W = np.random.randn(nx, 1)
-    b = 0
-    dW_prev1 = np.zeros((nx, 1))
-    db_prev1 = 0
-    dW_prev2 = np.zeros((nx, 1))
-    db_prev2 = 0
-    for i in range(1000):
-        A = forward_prop(X, W, b)
-        if not (i % 100):
-            cost = calculate_cost(Y, A)
-            print('Cost after {} iterations: {}'.format(i, cost))
-        dW, db = calculate_grads(Y, A, W, b)
-        W, dW_prev1, dW_prev2 = update_variables_Adam(0.001, 0.9, 0.99, 1e-8, W, dW, dW_prev1, dW_prev2, i + 1)
-        b, db_prev1, db_prev2 = update_variables_Adam(0.001, 0.9, 0.99, 1e-8, b, db, db_prev1, db_prev2, i + 1)
-    A = forward_prop(X, W, b)
-    cost = calculate_cost(Y, A)
-    print('Cost after {} iterations: {}'.format(1000, cost))
+    # bias correction
+    v_corrected = new_v / (1 - beta1**t)
+    s_corrected = new_s / (1 - beta2**t)
 
-    Y_pred = np.where(A >= 0.5, 1, 0)
-    fig = plt.figure(figsize=(10, 10))
-    for i in range(100):
-        fig.add_subplot(10, 10, i + 1)
-        plt.imshow(X_3D[i])
-        plt.title(str(Y_pred[i, 0]))
-        plt.axis('off')
-    plt.tight_layout()
-    plt.show()
+    # update var
+    var = var - alpha * (v_corrected / (np.sqrt(s_corrected) + epsilon))
+
+    return var, new_v, new_s
