1b1b421289f9c235ded4610a33ac9fef13c56019
sindi16 <sindibardhi078@gmail.com>
2026-05-08 19:23:57 +0200

    hi

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -9,7 +9,9 @@ import gensim
 def word2vec_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    epochs=5, seed=0, workers=1):
-
+    """
+    Creates and trains a Word2Vec model
+    """
     sg = 0 if cbow else 1
 
     model = gensim.models.Word2Vec(
